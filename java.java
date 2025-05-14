32c4391b2a71257345607a14aa8007dbc859211b
HoriZon-05 <2115677340@qq.com>
2025-05-14 14:41:59 +0800

    关闭确认弹窗,设置一键推送

--- java.java
+++ java.java
@@ -2,6 +2,7 @@ import org.w3c.dom.ls.LSOutput;
 
 public class java {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
+        System.out.println("Hello git!");
     }
 }
